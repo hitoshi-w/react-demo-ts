bd523609661a26e74002d9a8e837365a689177ca
Hitoshi Watanabe <h_watanabe@HitoshinoMacBook-Pro.local>
2020-02-05 15:43:01 +0900

    Set up firestore and redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import 'firebase/firestore';
+import fb from 'config/fbConfig';
+
 import App from './App';
+import { rootReducer } from 'reducers/rootReducer';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export const db = fb.firestore();
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
